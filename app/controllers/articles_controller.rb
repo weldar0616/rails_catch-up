1431570d4aa65e6ccf87100d64b5472cb76ba71d
ogata <ogata@beaglee.com>
2022-09-24 15:40:59 +0900

    11章 セキュリティ

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'dhh', password: 'secret', except: %i[index show]
+
   def index
     @articles = Article.all
   end
@@ -43,7 +45,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :body, :status)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
 end
